177225c02ff9dd77dff626320e4d49bac82a1053
AmandaVillamizar <villamizaramanda2@gmail.com>
2024-10-26 16:36:00 -0400

    Convertidor
    Actividad

---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub btnPrestar_Click(sender As Object, e As EventArgs) Handles btnPrestar.Click
+        Dim prestarForm As New FormPrestar()
+        prestarForm.Show()
+    End Sub
+
+    Private Sub btnDevolver_Click(sender As Object, e As EventArgs) Handles btnDevolver.Click
+        Dim devolverForm As New FormDevolver()
+        devolverForm.Show()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
